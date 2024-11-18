9f92c2074ce652dc1f7515a7be42a697607a24d2
yangsenessa <yang82101@163.com>
2024-11-18 20:55:33 +0800

    with vmc

--- src/mugc-agc-backend/src/lib.rs
+++ src/mugc-agc-backend/src/lib.rs
@@ -7,13 +7,7 @@ use std::mem;
 use icrc_ledger_types::icrc1::account::Account;
 use icrc_ledger_types::icrc1::transfer::{BlockIndex, NumTokens};
 use icrc_ledger_types::icrc2::transfer_from::{TransferFromArgs, TransferFromError};
-use icrc_nft_types::{
-    icrc7::{
-        metadata::{Icrc7CollectionMetadata, Icrc7TokenMetadata},
-        transfer::{TransferArg, TransferResult},
-    },
-    Account,
-};
+
 use serde::Serialize;
 
 
@@ -124,75 +118,6 @@ fn update_minting_contract(args:WorkLoadInitParam)->Option<WorkLoadInitParam> {
     })
 
 }
-
-#[ic_cdk::update]
-async fn transfer(args: TransferArgs) -> Result<BlockIndex, String> {
-    ic_cdk::println!(
-        "Transferring {} tokens to account {}",
-        &args.amount,
-        &args.to_account,
-    );
-
-    let transfer_from_args = TransferFromArgs {
-        // the account we want to transfer tokens from (in this case we assume the caller approved the canister to spend funds on their behalf)
-        from: Account::from(ic_cdk::caller()),
-        // can be used to distinguish between transactions
-        memo: None,
-        // the amount we want to transfer
-        amount: args.amount,
-        // the subaccount we want to spend the tokens from (in this case we assume the default subaccount has been approved)
-        spender_subaccount: None,
-        // if not specified, the default fee for the canister is used
-        fee: None,
-        // the account we want to transfer tokens to
-        to: args.to_account,
-        // a timestamp indicating when the transaction was created by the caller; if it is not specified by the caller then this is set to the current ICP time
-        created_at_time: None,
-    };
-
-    // 1. Asynchronously call another canister function using `ic_cdk::call`.
-    ic_cdk::call::<(TransferFromArgs,), (Result<BlockIndex, TransferFromError>,)>(
-        // 2. Convert a textual representation of a Principal into an actual `Principal` object. The principal is the one we specified in `dfx.json`.
-        //    `expect` will panic if the conversion fails, ensuring the code does not proceed with an invalid principal.
-        Principal::from_text("mxzaz-hqaaa-aaaar-qaada-cai")
-            .expect("Could not decode the principal."),
-        // 3. Specify the method name on the target canister to be called, in this case, "icrc1_transfer".
-        "icrc2_transfer_from",
-        // 4. Provide the arguments for the call in a tuple, here `transfer_args` is encapsulated as a single-element tuple.
-        (transfer_from_args,),
-    )
-    .await // 5. Await the completion of the asynchronous call, pausing the execution until the future is resolved.
-    // 6. Apply `map_err` to transform any network or system errors encountered during the call into a more readable string format.
-    //    The `?` operator is then used to propagate errors: if the result is an `Err`, it returns from the function with that error,
-    //    otherwise, it unwraps the `Ok` value, allowing the chain to continue.
-    .map_err(|e| format!("failed to call ledger: {:?}", e))?
-    // 7. Access the first element of the tuple, which is the `Result<BlockIndex, TransferError>`, for further processing.
-    .0
-    // 8. Use `map_err` again to transform any specific ledger transfer errors into a readable string format, facilitating error handling and debugging.
-    .map_err(|e: TransferFromError| format!("ledger transfer error {:?}", e))
-}
-
-#[ic_cdk::query]
-async fn query_poll_balance()->Result<NumTokens,String> {
-    ic_cdk::println!(
-        "Query balance of mining pool {}",
-        ic_cdk::id(),
-    );
-
-    let balance = ic_cdk::call::<(Account,),(Nat,)> (
-        Principal::from_text("mxzaz-hqaaa-aaaar-qaada-cai")
-                       .expect("Could not decode the principal."),
-        "icrc1_balance_of",
-        (Account::from(ic_cdk::id()),)
-
-    ).await
-    .map_err(|e| format!("fail to call ledger:{:?}",e))?
-    .0.clone();
-
-    Ok(balance)
-     
-}
-
 #[ic_cdk::update]
 fn push_workload_record(record:ComfyUIPayload) ->Result<BlockIndex,MixComfyErr>{
     ic_cdk::println!("Push work load:{:?}", record);
@@ -204,6 +129,7 @@ fn push_workload_record(record:ComfyUIPayload) ->Result<BlockIndex,MixComfyErr>{
     })
 }
 
+
 #[ic_cdk::query]
 fn query_curr_workload() ->Option<Vec<WorkLoadLedgerItem>>{
     ic_cdk::println!("Query all workload");
@@ -213,9 +139,6 @@ fn query_curr_workload() ->Option<Vec<WorkLoadLedgerItem>>{
         state.mixcomfy.query_all_workload()
     })
 }
-
-
-
 
 // Enable Candid export (see https://internetcomputer.org/docs/current/developer-docs/backend/rust/generating-candid)
 ic_cdk::export_candid!();
